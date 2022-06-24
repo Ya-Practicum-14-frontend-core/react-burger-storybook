49b570f4f74f9dc17d647808a1e3370712db668c
А В <toshig@yandex.ru>
2022-06-24 16:41:16 +0300

    add EmailInput storybook

---
+++ packages/ui/src/email-input/EmailInput.tsx
@@ -0,0 +1,21 @@
+import { EmailInput as YA_EmailInput} from '@ya.praktikum/react-developer-burger-ui-components';
+import { ChangeEvent } from 'react';
+
+
+import './EmailInput.css';
+
+export interface IEmailInputProps {
+  value: string;
+  name: string;
+  size?: "small" | "default" | undefined;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export function EmailInput(props: IEmailInputProps): JSX.Element {
+  return (
+    <YA_EmailInput {...props} />
+  );
+};
