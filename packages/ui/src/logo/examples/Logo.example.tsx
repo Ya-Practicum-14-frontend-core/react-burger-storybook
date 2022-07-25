25f549cccb4e7a50880918bd72c0aade435a7ebb
StepanovKirill <st.kirilius@mail.ru>
2022-07-25 20:19:38 +0500

    logo component added

---
+++ packages/ui/src/logo/examples/Logo.example.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+
+export const LogoExample = () => {
+  return (
+    <>
+      <Logo />
+    </>
+  );
+};
