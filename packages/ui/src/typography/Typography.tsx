23844f8c3912d332cfa5746b30bf82411f5e6a9f
hardaoko <hardaoko@gmail.com>
2022-07-26 00:12:27 +0300

    Первая итерация

---
+++ packages/ui/src/typography/Typography.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react'
+
+export interface ITypographyProps {
+  /**
+  Цвет текста
+  */
+  color: 'text_color_inactive' | 'text_color_primary' | 'text_color_accent' |  'text_color_error' |  'text_color_success';
+  /**
+  Тип текста
+  */
+  type: 'text_type_main-default' | 'text_type_main-large' | 'text_type_main-medium' |
+    'text_type_main-small' |  'text_type_digits-default' | 'text_type_digits-medium' |  'text_type_digits-large';
+  /**
+  Текст
+  */
+  value: string;
+}
+
+export const Typography: FC<ITypographyProps> = ({type, color, value}) => {
+  return <p className={`${type ? type : 'text_type_main-default'} ${color ? color : 'text_color_primary'}`}> {value ? value : "Пример текста"} </p>
+}
---
+++ packages/ui/src/typography/examples/Typography.example.tsx
@@ -0,0 +1,5 @@
+import { Typography } from "../Typography";
+
+export const TypographyExample = () => {
+    <Typography color="text_color_inactive" type="text_type_main-default" value="Пример текста"/>
+}
