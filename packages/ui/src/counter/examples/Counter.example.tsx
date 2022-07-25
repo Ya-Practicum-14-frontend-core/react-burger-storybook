ce98ad7ffdc7c93d68b2835df48d404952731d4b
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2022-07-25 15:34:25 +0500

    Скорректировал аргументы для EmailInput

--- packages/ui/src/counter/examples/Counter.example.tsx
+++ packages/ui/src/counter/examples/Counter.example.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Counter } from "@ya.praktikum/react-developer-burger-ui-components";
 
-export const CounterExample = () => {
-    const [count, setCount] = React.useState<number>(0);
-
-    return <Counter count={count} size={'default'} />;
+export const CounterExample = (props: {count: number; size: 'small' | 'default' | undefined} ) => {
+    return (
+      <div style={{position:'relative', padding:20}}>
+          <Counter count={props.count} size={props.size} />
+      </div>
+    );
 };
