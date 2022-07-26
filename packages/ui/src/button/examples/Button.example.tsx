7eb2c8657d44eb5009e9c390b795fb0e563ba7d7
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2022-07-26 14:59:19 +0500

    Кнопка

--- packages/ui/src/button/examples/Button.example.tsx
+++ packages/ui/src/button/examples/Button.example.tsx
@@ -1,9 +1,11 @@
+// @ts-nocheck
+// В версии React для @ya.praktikum/react-developer-burger-ui-components и последующих версиях существует конфликт поэтому используем ts-ignore
 import {useState, FC} from "react";
 import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
 import { IButtonProps } from "../Button";
 
 export const ButtonExample = () => {
   return (
-    <Button name="btnName" type="primary" size="medium"></Button>
+    <Button onClick={console.log("Hello, World !")} name="btnName" type="primary" size="medium">Кнопка</Button>
     )
 }
